545fb426cfd7a260121de72daf0779a940616ec7
nasseralikarimi <nasser.karimi@cloudelligent.com>
2022-07-14 12:35:16 +0430

    init enviroment and set the basic backend and frontend connection

---
+++ frontend/src/router.js
@@ -0,0 +1,12 @@
+import { createWebHistory, createRouter } from "vue-router";
+ 
+// import App from './App.vue'
+   
+const routes = [
+];
+ 
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
