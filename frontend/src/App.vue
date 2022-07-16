fd6c166ee8b9c404091fcf4d8c650da2c0afab92
nasseralikarimi <nasser.karimi@cloudelligent.com>
2022-07-16 20:02:30 +0430

    add the nav bar to the top of pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <div id="app" class="container is-max-desktop">
-    <router-view />
+  <div id="app" class="container is-max-desktop bg-light">
+    <div class="row">
+      <navbar></navbar>
+    </div>
+    <hr />
+    <div class="row">
+      <router-view />
+    </div>
   </div>
 </template>
- 
+
 <script>
+import NavBar from "./components/shared/NavBar.vue";
+
 export default {
   name: "App",
+  components: {
+    navbar: NavBar,
+  },
 };
 </script>
- 
+
 <style>
 /* import style bulma */
 @import "~bulma/css/bulma.css";
